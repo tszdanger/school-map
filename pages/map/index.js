787838221efe74cf2910afd7bcfcba7880b022e4
skywf <1142383654@qq.com>
2020-04-29 22:45:15 +0800

    hitsz框架初步

--- pages/map/index.js
+++ pages/map/index.js
@@ -3,8 +3,8 @@ var app = getApp();
 Page({
   data: {
     fullscreen: false,
-    latitude: 22.973132,
-    longitude: 113.755875,
+    latitude: 22.585947,
+    longitude: 113.973297,
     buildlData: app.globalData.map,
     windowHeight: "",
     windowWidth: "",
